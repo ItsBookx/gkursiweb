0cbf127de345ad3443e61fae66473aa3f53ff560
ItsBookx <100990277+ItsBookx@users.noreply.github.com>
2022-11-25 13:34:51 +0300

    updated code

--- web/def/js/debugger.js
+++ web/def/js/debugger.js
@@ -1,11 +1,14 @@
-let developement = true;
+let developement = false;
 
 function defs(developement) {
+    let s=document.styleSheets;
     if(developement==true){
-        console.log('Developer mode detected');
+        console.log('Developer mode detected, enabling debugger.css');
+        s[1].disabled=false;
     }
     else {
-        console.log('User mode detected');
+        console.log('User mode detected, disabling debugger.css');
+        s[1].disabled=true;
     }
 };
 
